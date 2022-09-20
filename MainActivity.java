8b87ddc69137fd75d6afdffb874deda38b1612fc
PARKJUNYOUNGKR <96164365+PARKJUNYOUNGKR@users.noreply.github.com>
2022-09-20 13:39:35 +0900

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    Button button1, button2, button3, button4;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout
+                .activity_main);
+        button1 = (Button) findViewById(R.id.button1);
+        button1.setBackgroundColor(Color.GREEN);
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setBackgroundColor(Color.RED);
+
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setBackgroundColor(Color.RED);
+
+        button4 = (Button) findViewById(R.id.button4);
+        button4.setBackgroundColor(Color.RED);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "버튼을 눌렀어요",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Toast.makeText(getApplicationContext(), "살려주세요",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(getApplicationContext(), "사진을 보여주세요",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            finish();
+            }
+        });
+    }
+
+}
